ca2e3c978fee252ceabe7557ca12bd6af21aad4b
xeyzo <schneidergw@yahoo.com>
2020-08-27 08:57:50 +0700

    get post data

---
+++ src/components/PostList.vue
@@ -0,0 +1,12 @@
+<template>
+<div class="bg-gray-200 p-4">
+  <p>{{ body }}</p>
+</div>
+</template>
+
+<script>
+export default {
+    name:"PostList",
+    props:["userId", "id", "title", "body"],
+}
+</script>
